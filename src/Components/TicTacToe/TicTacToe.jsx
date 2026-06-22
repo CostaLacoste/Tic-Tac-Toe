b23e1108196e718bef11e2e7dd36665d4f2505aa
bogdanm <cwdw@icloud.com>
2026-06-22 14:28:31 +0400

    Second

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './TicTacToe.css'
+import circle_icon from '../Assets/circle.png'
+import cross_icon from '../Assets/cross.png'
+
+const TicTacToe = () => {
+    return (
+        <div className="container">
+            <h1 className="titile">Tic Tac Toe Game</h1>
+            <div className="board">
+                
+            </div>
+        </div>
+    )
+}
+
+export default TicTacToe
